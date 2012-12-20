b711440e462cbb8c84935b1296d1f56331d77c33
ryu22e <ryu22e@gmail.com>
2012-12-21 01:09:32 +0900

    このクラスをgit addし忘れていた :sweat:
    close #5

---
+++ src/main/java/junit/tutorial/User.java
@@ -0,0 +1,13 @@
+package junit.tutorial;
+
+public class User {
+
+    public String getName() {
+        return "nobody";
+    }
+
+    public boolean isAdmin() {
+        return false;
+    }
+
+}
