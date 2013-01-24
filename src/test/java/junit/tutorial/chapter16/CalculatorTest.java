238badac1d9493060fa1d0d2244db678f58f1954
ryu22e <ryu22e@gmail.com>
2013-01-25 00:17:02 +0900

    ソースコードをEclipseでフォーマットした。
    ref #15

--- src/test/java/junit/tutorial/chapter16/CalculatorTest.java
+++ src/test/java/junit/tutorial/chapter16/CalculatorTest.java
@@ -9,6 +9,6 @@ public class CalculatorTest {
     @Test
     public void addは3と4で7を返す() throws Exception {
         Calculator sut = new Calculator();
-        assertThat(sut.add(3,4),is(7));
+        assertThat(sut.add(3, 4), is(7));
     }
 }
