1144c3c75cfc8bc3c5c5feaf778072975ff576cc
ryu22e <ryu22e@gmail.com>
2013-01-25 00:20:17 +0900

    プロダクションコードに本実装を書いた。
    close #15

--- src/main/java/junit/tutorial/chapter16/Calculator.java
+++ src/main/java/junit/tutorial/chapter16/Calculator.java
@@ -3,8 +3,7 @@ package junit.tutorial.chapter16;
 public class Calculator {
 
     public int add(int x, int y) {
-        // TODO これは仮実装なので、後で本実装を書く。
-        return 7;
+        return x + y;
     }
 
 }
