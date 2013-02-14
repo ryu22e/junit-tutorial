7c1790b6849a38699053d12fdd2934dcf1a4684a
ryu22e <ryu22e@gmail.com>
2013-02-15 00:16:27 +0900

    ポーカーゲームの受け入れテストクラスを追加した。
    ref #16

---
+++ src/test/java/cucumber/poker/PokerGameAcceptanceTest.java
@@ -0,0 +1,11 @@
+package cucumber.poker;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format = { "pretty" }, monochrome = true)
+public class PokerGameAcceptanceTest {
+
+}
